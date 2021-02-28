383da77cd7b803ba1218e324423d458f41c4fd12
Kamonabe <ryo79158420k@icloud.com>
2021-02-28 22:43:29 +0900

    4-21

--- index.js
+++ index.js
@@ -19,4 +19,21 @@ function getC() {
 }
 
 // TODO ここに getA, getB, getC で得られる結果をかけあわせた結果 2431 を標準出力するコードを記述する
+getA().then(resA=>{
+  return getB().then(resB=>{
+    return resA*resB;
+  })
+})
+.then(resAB=>{
+  getC().then(resC=>{
+    console.log(resAB*resC);
+  })
+})
 // ただし Promise チェイン(then関数の結果に対するthen関数の呼び出し)を一度は用いて実装をすること
+
+//これでも
+/*
+Promise.all([getA(), getB(), getC()]).then(results => {
+  console.log(results[0] * results[1] * results[2]);
+});
+*/
